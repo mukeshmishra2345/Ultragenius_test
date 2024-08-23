3464a8e1e065645a4235e95f7a9f8faf09b2842f
Mukesh Mishra <mukeshmishra2345@gmail.com>
2024-08-23 20:52:46 +0530

    test cases for multiple numbers

--- func_test.py
+++ func_test.py
@@ -3,9 +3,7 @@ from calculator import add
 
 # When the input is an empty string, the method should return 0
 def test_empty_string_returns_zero():
-    result = add("")
-    print(f"Test Empty String: Expected 0, Got {result}")
-    assert result == 0
+    assert add("") == 0
     
 # Handle a single number like "1" (return 1) and two numbers like "1,2" (return 3).
 def test_single_number():
@@ -13,3 +11,7 @@ def test_single_number():
 
 def test_two_numbers():
     assert add("1,2") == 3
+
+# Test for multiple comma separated numbers
+def test_multiple_numbers():
+    assert add("1,2,3,4") == 10
